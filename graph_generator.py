3d27308be63f430be2e1369bd06c08c4208ce094
mustafa-be <61984522+mustafa-be@users.noreply.github.com>
2020-07-26 20:22:42 +0500

    Initial Commit

---
+++ graph_generator.py
@@ -0,0 +1,34 @@
+import pickle
+from networkx.generators.random_graphs import erdos_renyi_graph
+import networkx as nx
+import matplotlib.pyplot as plt
+def generate_connected_graph(nds,p):
+    n = nds
+    i = 0
+    while(1):
+        G = erdos_renyi_graph(n, p)
+        # print("Iterator: ",i)
+        # print(G.nodes)
+        # print(G.edges)
+        # print(nx.is_connected(G)) #return true if all nodes in geaph connected
+        # nx.draw_networkx(G)
+        # plt.show()
+        if(nx.is_connected(G)):
+            break;
+        i = i + 1
+        # print('\n------------------------\n')
+    return G
+
+
+n = 10
+p = 0.4
+#G = erdos_renyi_graph(n, p)
+G = generate_connected_graph(n,0.2)
+print(list(G.edges))
+#pos = nx.spring_layout(G)
+#nx.draw_networkx(G, pos)
+#plt.show()
+f = open('store.pckl', 'wb')
+pickle.dump(G, f)
+f.close()
+
